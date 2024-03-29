f2f268724843f868af0ec60a433783c36d132466
Rodrigo Albuquerque <rodrigoalbuquerque.ti@gmail.com>
2024-03-29 14:25:42 -0300

    Primeorp commit - todos os arquivos

---
+++ basico/ola.php
@@ -0,0 +1,13 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Exemplo A</h2>
+<?php 
+    echo 'Olá <br> ';
+    echo 'Mundo!'
+?>
+<h2>Exemplo B</h2>
+<?= "OUTRA FORMA DE ME COMUNICAR ?" ?>
+
+<h2>Informações</h2>
+<?php
+    phpinfo();
